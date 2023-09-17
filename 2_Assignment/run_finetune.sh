f9dc442c0aeff60b521bc0c37b03ad465324b795
Sergio Remigio <u1217882@notchpeak2.int.chpc.utah.edu>
2023-09-17 10:44:41 -0600

    Imported database for assigment 2

--- 2_Assignment/run_finetune.sh
+++ 2_Assignment/run_finetune.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition soc-gpu-np
 #SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:a100:1 
+#SBATCH --gres=gpu:1 
 #SBATCH --time=8:00:00
-#SBATCH --mem=40GB
+#SBATCH --mem=80GB
 #SBATCH --mail-user=u1217882@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
 #SBATCH -o assignment_1-%j
@@ -15,9 +15,9 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate environment
 
 mkdir -p /scratch/general/vast/1217882/huggingface_cache
-export TRANSFORMER_CACHE="/scratch/general/vast/u1217882/huggingface_cache"
+export TRANSFORMERS_CACHE="/scratch/general/vast/u1217882/huggingface_cache"
 export HF_DATASETS_CACHE="/scratch/general/vast/u1217882/huggingface_cache"
 
 OUT_DIR=/scratch/general/vast/u1217882/cs6966/assignment1/models
 mkdir -p ${OUT_DIR}
-python training_file.py --output_dir ${OUT_DIR} --batch_size 16 --learning_rate 5e-5 --weight_decay 0.01
+python training_file.py 
--- 2_Assignment/training_file.py
+++ 2_Assignment/training_file.py
@@ -1,130 +1,53 @@
 import os
-import jsonlines
-import random
 import torch
-import torch.nn as nn
-import argparse
+from PIL import Image
+from transformers import IdeficsForVisionText2Text, AutoProcessor
 from datasets import load_dataset
-from transformers import DebertaV2Tokenizer, Trainer, TrainingArguments, AutoModel
 
-# Parse output directory arguments,
-parser = argparse.ArgumentParser()
-parser.add_argument('--output_dir', type=str, help='Directory where model checkpoints will be saved')
-parser.add_argument('--batch_size', type=int, help='Batch size for training', default=2)
-parser.add_argument('--epochs', type=int, help='Number of training epochs', default=1)
-parser.add_argument('--learning_rate', type=float, help='Learning rate for training', default=2e-5)
-parser.add_argument('--weight_decay', type=float, help='Weight decay for regularization', default=0.01)
-parser.add_argument('--metric_for_best_model', type=str, help='The metric to use to compare two different models', default="accuracy")
-args = parser.parse_args()
+# Get the training dataset
+dataset = load_dataset("jmhessel/newyorker_caption_contest", split="train")
 
+# Set the random seed for reproducibility
+torch.manual_seed(1217882)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(1217882)
 
-class DebertaClassificationHead(nn.Module):
-    def __init__(self, num_labels, hidden_dim):
-        super().__init__()
-        self.dense = nn.Linear(hidden_dim, hidden_dim)
-        self.dropout = nn.Dropout(0.1)
-        self.out_proj = nn.Linear(hidden_dim, num_labels)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    def forward(self, x):
-        x = self.dense(x)
-        x = nn.ReLU()(x)
-        x = self.dropout(x)
-        x = self.out_proj(x)
-        return x
+checkpoint = "HuggingFaceM4/idefics-9b-instruct"
+model = IdeficsForVisionText2Text.from_pretrained(checkpoint, torch_dtype=torch.bfloat16).to(device)
+processor = AutoProcessor.from_pretrained(checkpoint)
 
-class DebertaForIMDBClassification(nn.Module):
-    def __init__(self, num_labels=2):
-        super(DebertaForIMDBClassification, self).__init__()
-        self.deberta = AutoModel.from_pretrained("microsoft/deberta-v3-base")
-        self.classifier = DebertaClassificationHead(num_labels, self.deberta.config.hidden_size)
+# Get the current working directory
+current_directory = os.getcwd()
 
-    def forward(self, input_ids, attention_mask=None, labels=None):
-        outputs = self.deberta(input_ids, attention_mask=attention_mask)
-        pooled_output = outputs.last_hidden_state[:, 0]  # Take the hidden state of the first token ([CLS])
-        logits = self.classifier(pooled_output)
+# We feed to the model an arbitrary sequence of text strings and images. Images can be either URLs or PIL Images.
+prompts = [
+    [
+        "User: What is in this image?",
+        image_astronaut,
+        "<end_of_utterance>",
 
-        if labels is not None:
-            loss_fn = nn.CrossEntropyLoss()
-            loss = loss_fn(logits, labels)
-            return loss, logits
-        else:
-            return logits
+        "\nAssistant: This picture depicts Idefix, the dog of Obelix in Asterix and Obelix. Idefix is running on the ground.<end_of_utterance>",
 
+        "\nUser:",
+        "https://static.wikia.nocookie.net/asterix/images/2/25/R22b.gif/revision/latest?cb=20110815073052",
+        "And who is that?<end_of_utterance>",
 
-model = DebertaForIMDBClassification()
-model_name = "deberta-v3-base"
-task = 'imdb'
+        "\nAssistant:",
+    ],
+]
 
-# Initialize DeBERTa tokenizer
-tokenizer = DebertaV2Tokenizer.from_pretrained('microsoft/deberta-v3-base')
+# --batched mode
+inputs = processor(prompts, add_end_of_utterance_token=False, return_tensors="pt").to(device)
+# --single sample mode
+# inputs = processor(prompts[0], return_tensors="pt").to(device)
 
-# Tokenization
-def tokenize_batch(batch):
-    return tokenizer(batch['text'], padding=True, truncation=True, max_length=512)
+# Generation args
+exit_condition = processor.tokenizer("<end_of_utterance>", add_special_tokens=False).input_ids
+bad_words_ids = processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
 
-# Load IMDB dataset
-dataset = load_dataset('imdb')
-
-"""
-# Split the training set into training and validation
-train_dataset = dataset['train'].train_test_split(test_size=0.1)  # 10% for validation
-train_data = train_dataset['train'].map(tokenize_batch, batched=True)
-validation_data = dataset['test'].map(tokenize_batch, batched=True)
-"""
-
-train_data = dataset['train'].map(tokenize_batch, batched=True)
-test_data = dataset['test'].map(tokenize_batch, batched=True)
-
-# Training Arguments
-training_args = TrainingArguments(
-    os.path.join(args.output_dir, f"{model_name}-finetuned-{task}"),
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
-    learning_rate=args.learning_rate,
-    per_device_train_batch_size=args.batch_size,
-    per_device_eval_batch_size=args.batch_size,
-    num_train_epochs=args.epochs,
-    weight_decay=args.weight_decay,
-    gradient_accumulation_steps=8,  # Number of updates steps to accumulate before performing a backward/update pass
-    load_best_model_at_end=True,
-    metric_for_best_model=args.metric_for_best_model,
-)
-
-# Function to compute metrics
-def compute_metrics(p):
-    return {"accuracy": (p.predictions.argmax(-1) == p.label_ids).mean()}
-
-
-# Initialize Trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_data,
-    eval_dataset=test_data,  
-    compute_metrics=compute_metrics,
-)
-
-# Training and Evaluation
-trainer.train()
-trainer.evaluate()
-
-# Predictions on test data
-predictions = trainer.predict(test_data)
-predicted_labels = predictions.predictions.argmax(-1)
-
-# Actual labels
-actual_labels = [x['label'] for x in test_data]
-
-# Incorrectly classified instances
-incorrect_indices = [i for i, (true, pred) in enumerate(zip(actual_labels, predicted_labels)) if true != pred]
-
-# Randomly select 10 incorrect instances
-selected_indices = random.sample(incorrect_indices, min(10, len(incorrect_indices)))
-
-# Detokenize and save these instances into a jsonlines file
-output_items = [{"review": tokenizer.decode(test_data["input_ids"][i]), "label": int(actual_labels[i]), "predicted": int(predicted_labels[i])} for i in selected_indices]
-
-    
-with jsonlines.open('incorrect_instances.jsonl', mode='w') as writer:
-    for item in output_items:
-        writer.write(item)
+generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+for i, t in enumerate(generated_text):
+    print(f"{i}:\n{t}\n")
